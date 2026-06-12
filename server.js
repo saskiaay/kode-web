7318ee531516dab9a02ecd44c8303094ccb262f1
saskiaay <saskdw@gmail.com>
2026-06-12 01:50:15 -0700

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Contoh penggunaan di rute
 app.get('/api/test-db', (req, res) => {
   db.query('SELECT 1', (err, results) => {
