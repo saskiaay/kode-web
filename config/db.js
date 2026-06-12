14edb0f8d8cc8871f2146c3490a21b6c8a2945d6
saskiaay <saskdw@gmail.com>
2026-06-11 20:22:37 -0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,18 +4,17 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-// Mencegah error ECONNREFUSED saat di Railway dengan mengecek DB_HOST
 const isProduction =
   process.env.NODE_ENV === 'production' || process.env.DB_HOST?.includes('railway');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || '127.0.0.1', // Pakai 127.0.0.1 (IPv4) jika localhost agar tidak terbaca ::1
+  host: process.env.DB_HOST || '127.0.0.1', 
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'railway',
   port: parseInt(process.env.DB_PORT || '3306'),
   waitForConnections: true,
-  connectionLimit: isProduction ? 5 : 10, // Menyesuaikan limit koneksi di cloud
+  connectionLimit: isProduction ? 5 : 10, 
   queueLimit: 0,
   connectTimeout: 10000,
 });
